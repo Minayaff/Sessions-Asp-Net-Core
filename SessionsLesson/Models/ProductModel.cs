7e0ddc2e6710fd6c579e071e8f3f689e8dbcaa41
Minayaff <minayaff@code.edu.az>
2020-06-25 17:10:34 +0400

    sdfd

---
+++ SessionsLesson/Models/ProductModel.cs
@@ -0,0 +1,48 @@
+﻿using SessionsLesson.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SessionsLesson.Models
+{
+    public class ProductModel
+    {
+        private List<Product> Products;
+
+        public ProductModel()
+        {
+            Products = new List<Product>() {
+                new Product
+                {
+                    Id = "p01",
+                    Name = "name 1",
+                    Price = 4,
+                   Photo = "iphon.jpg"
+                },
+                new Product
+                {
+                    Id = "p02",
+                    Name = "name 2",
+                    Price = 2,
+                   Photo = "iphon.png"
+                },
+                new Product
+                {
+                    Id = "p03",
+                    Name = "name 3",
+                    Price = 8,
+                    Photo = "iphones.png"
+                }
+            };
+        }
+
+        public List<Product> findAll()
+        {
+            return Products;
+        }
+
+        public Product find(string id)
+        {
+            return Products.Where(p => p.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ SessionsLesson/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SessionsLesson.Models;
+
+namespace SessionsLesson
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSession(options => {
+                options.Cookie.Name = ".Cookie";
+                options.IdleTimeout = TimeSpan.FromMinutes(2);
+                options.Cookie.IsEssential = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSession();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Demo}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
